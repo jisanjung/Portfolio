2f3f52d0cd1a36fa314c03d00a271b7dbb3fc6a9
Jisan Jung <jjjung13@gmail.com>
2020-09-10 21:42:35 -0400

    finished styling contact page

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,11 @@
 $(document).ready(function() {
-    
+
+    // contact page, map
+    var latLong = [39.95, -75.1619];
+    var mymap = L.map('mapid').setView(latLong, 13);
+        
+    L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="http://osm.org/           copyright">OpenStreetMap</a> contributors'
+    }).addTo(mymap);
+    var marker = L.marker(latLong).addTo(mymap);
 });
