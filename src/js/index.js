7e063ba8b158f99039b3639f0166f2ace8d2dd33
Jisan Jung <jjjung13@gmail.com>
2020-09-12 01:26:53 -0400

    nav link activation on scroll

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,51 @@
 $(document).ready(function() {
 
+    // function calls
+    smoothScroll();
+    setMap();
+    activeLink();
+
+    // smooth scroll
+    function smoothScroll() {
+        $('a[href*=\\#]').on('click', function(e){     
+            e.preventDefault();
+            $('html,body').animate({scrollTop:$(this.hash).offset().top}, 800);
+        });
+    }
+
     // contact page, map
-    var latLong = [39.95, -75.1619];
-    var mymap = L.map('mapid').setView(latLong, 13);
-        
-    L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="http://osm.org/           copyright">OpenStreetMap</a> contributors'
-    }).addTo(mymap);
-    var marker = L.marker(latLong).addTo(mymap);
+    function setMap() {
+        var latLong = [39.95, -75.1619];
+        var mymap = L.map('mapid').setView(latLong, 13);
+            
+        L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="http://osm.org/           copyright">OpenStreetMap</a> contributors'
+        }).addTo(mymap);
+        var marker = L.marker(latLong).addTo(mymap);
+    }
+
+    // activate nav link
+    function activeLink() {
+        var navLinks = $(".nav-link");
+
+        $(window).on("scroll", function(e) {
+            var fromTop = $(window).scrollTop();
+            navLinks.each(function() {
+                var currentLink = $(this);
+                var element = $(currentLink.attr("href"));
+
+                /*
+                when the current scroll position is greater than the current element
+                and the scroll position goes past the element position
+                activate link
+                otherwise, remove activation
+                */
+                if (element.position().top <= fromTop && element.position().top + element.height() > fromTop) {
+                    currentLink.addClass("active");
+                } else {
+                    currentLink.removeClass("active");
+                }
+            });
+        })
+    }
 });
